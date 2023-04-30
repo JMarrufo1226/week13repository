00be50a9e4dc04ffee55eff82c14a55b1838a954
JMarrufo1226 <joseamarrufo1200@gmail.com>
2023-04-29 17:38:44 -0700

    fixed navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,7 +12,7 @@ export default function() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">Links</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Dummy Link 1</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
